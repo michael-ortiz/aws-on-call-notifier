c718bf884390397387b6e6cd378069c85f4a9f7e
Michael Ortiz <michael.ortiz@evertecinc.com>
2021-05-22 19:16:48 -0400

    Adds SAM local configuration to test lambda

--- functions/lambda-notifier/index.ts
+++ functions/lambda-notifier/index.ts
@@ -20,14 +20,14 @@ export const handler = async (event: any = {}): Promise<any> => {
     const schedule: Schedule = await new GetScheduleUseCase(dynamoDB).execute();
     const user: User = await new GetUserUseCase(schedule, dynamoDB).execute();
 
-    const didSentEmail: boolean = await new SendEmailUseCase(user, ses).execute();
+    const didSendEmail: boolean = await new SendEmailUseCase(user, ses).execute();
     const didSendSms: boolean = await new SendSmsUseCase(user, sns).execute();
 
     return {
         user,
         schedule,
         sentNotifications: {
-            email: didSentEmail,
+            email: didSendEmail,
             sms: didSendSms
         }
     }
--- lib/settings/configurations.ts
+++ lib/settings/configurations.ts
@@ -3,25 +3,25 @@
  * Please enter your details in every <CHANGE ME>.
  */
 export const configurations = {
-    
+
     /**
-     * A cron expression, that denotes when the notification 
-     * lambda should be triggered in GMT timezone.
-     * 
-     * The given expression gets execcuted every day at 14:00:00 GMT.
-     */
-     SCHEDULE_CRON_EXPRESSION: "0 14 * * ? *",
+    * A cron expression, that denotes when the notification 
+    * lambda should be triggered in GMT timezone.
+    * 
+    * The given expression gets execcuted every day at 14:00:00 GMT.
+    */
+    SCHEDULE_CRON_EXPRESSION: "0 14 * * ? *",
 
     /**
      * A DynamoDB table that stores the users data and contact information.
      */
     USERS_TABLE: "on_call_users",
-    
+
     /**
      * A DynamoDB table that stores the on-call schedule.
      */
     SCHEDULE_TABLE: "on_call_schedule",
-    
+
     /**
      * The source email address that should appear in the message.
      * 
@@ -30,19 +30,19 @@ export const configurations = {
      * 
      */
     SOURCE_EMAIL: "<CHANGE ME>",
-    
+
     /**
      * The email message subjet for the notification
      */
     EMAIL_SUBJECT: "<CHANGE ME>",
-    
+
     /**
      * The email body for the notification. Supports HTML.
      */
     EMAIL_BODY: "<CHANGE ME>",
-    
+
     /**
      * The SMS message.
      */
-    SMS_MESSAGE: "<CHANGE ME>",
+    SMS_MESSAGE: "<CHANGE ME>"
 }
